7953020e7368f2f7891303899e36b9c6901b24a6
Austin Soohoo version 2 <austinsoohoo2@gmail.com>
2016-12-06 17:42:27 -0800

    did some work by copying stuff and stuff 12/6

--- src/org/usfirst/frc/team5026/robot/Constants.java
+++ src/org/usfirst/frc/team5026/robot/Constants.java
@@ -2,5 +2,15 @@ package org.usfirst.frc.team5026.robot;
 
 public class Constants {
 	
-	public static final float deadzone = 0.1f;
+	public static final float DEADZONE = 0.1f;
+	public static final int JOYSTICK_PORT = 0;
+	
+	public static final int LEFT_DRIVE_MOTOR_1 = 1;
+	public static final int LEFT_DRIVE_MOTOR_2 = 2; 
+	public static final int LEFT_DRIVE_MOTOR_3 = 3;
+	
+	public static final int RIGHT_DRIVE_MOTOR_1 = 1;
+	public static final int RIGHT_DRIVE_MOTOR_2 = 2; 
+	public static final int RIGHT_DRIVE_MOTOR_3 = 3;
+	
 }
